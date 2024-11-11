bb7598f52d6b7b766124f858f8614350ae2f8435
Lucas <l202071@dac.unicamp.br>
2024-11-11 17:02:40 -0300

    alterando estrutura do objeto com questoes

--- data/questions.js
+++ data/questions.js
@@ -1,14 +1,7 @@
-//Escrevam perguntas e respostas para o quizz 
-//Objetos para perguntas respostas e pontuação da resposta
-//Cada objeto é uma pergunta com suas respectivas respostas e pontuações
-//Cada resposta gera pontuação para os jogos que definimos
-
 const data = [
     {
         question: "Você joga LoL?",
-        options: ["Sim",
-                "Não"
-            ],
+        options: ["Sim", "Não"],
         games: {
             Acao: '1',
             Aventura: '0',
@@ -17,12 +10,11 @@ const data = [
             Puzzle: '0',
             Terror: '0',
             FPS: '0',
-        },
-
+        }
+    },
+    {
         question: "Você joga Minecraft?",
-        options: ["Sim",
-                "Não"
-            ],
+        options: ["Sim", "Não"],
         games: {
             Acao: '0',
             Aventura: '1',
@@ -31,21 +23,22 @@ const data = [
             Puzzle: '0',
             Terror: '0',
             FPS: '0',
-        },
-
-        //Replique o objeto acima para adicionar mais perguntas
-        //Exemplo:
-        //Question: 'Você gosta de jogos de tiro?'
-        //Options: ['Sim', 'Não']
-        //Games: {Acao: 1, 
-        //        Aventura: 0,
-        //        RPG: 0,
-        //        Estrategia: 0,
-        //        Puzzle: 0,
-        //        Terror: 0,
-        //        FPS: 1,
-        //    },
+        }
     },
+    {
+        question: "Você gosta de jogos de tiro?",
+        options: ["Sim", "Não"],
+        games: {
+            Acao: '1',
+            Aventura: '0',
+            RPG: '0',
+            Estrategia: '0',
+            Puzzle: '0',
+            Terror: '0',
+            FPS: '1',
+        }
+    }
+    
 ];
 
 export default data;
